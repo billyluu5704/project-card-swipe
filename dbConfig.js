58ffdf079c425ecb79fa10abc9d32c084e1d08f7
Billy Luu <luudang04@gmail.com>
2024-02-01 22:43:42 +0800

    Adjust in file dbConfig.js for database connection

--- dbConfig.js
+++ dbConfig.js
@@ -2,17 +2,12 @@ require("dotenv").config();
 
 const { Pool } = require('pg');
 
-/* const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`; */
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`; 
 
 const pool = new Pool({
-    //connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "#Dangquang49",
-    database: "variantz_user"
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
 });
 
 pool.connect();
